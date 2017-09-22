08cd757b7d9ac805d0072d76268736d8c993a001
mnnieto <mnnieto@gmail.com>
2017-09-22 12:35:06 +0200

    FIRST COMMIT

---
+++ prueba-bankinter-jenkins1/src/main/java/com/bankinter/arq/jenkins1/App.java
@@ -0,0 +1,13 @@
+package com.bankinter.arq.jenkins1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
